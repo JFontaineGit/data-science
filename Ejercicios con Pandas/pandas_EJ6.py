bece4c2e70697a2ed70b6b818e7902240e7e056f
Just Fontaine <itsmealeexvii@gmail.com>
2024-09-15 01:54:14 -0300

    .

--- Ejercicios con Pandas/pandas_EJ6.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('Ejercicios con Pandas/archivos/ventas.csv')
-
-# Agrupando por Tiendas y obteniendo las ventas totales de las mismas. Asignamos un alias a la columna resultante del total de ventas.
-ventas_totales = df.groupby('Tienda').sum()['Ventas'].rename('Total de Ventas')
-
-print(ventas_totales)
--- Ejercicios con Pandas/pandas_EJ7.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('Ejercicios con Pandas/archivos/empleados.csv')
-
-# Añadiendo una columa con el total del salario + la bonificacion. Ordenamos de mayor a menor.
-df['Total Pago'] = df['Salario'] + df['Bonificacion']
-print(df.sort_values(by="Total Pago", ascending=False))
